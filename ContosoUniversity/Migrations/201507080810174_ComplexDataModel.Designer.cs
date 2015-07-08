905a0628f5bcec51a13de8290e971cb5d5d2ac7a
Michał <michal.walaszczyk@gmail.com>
2015-07-08 15:39:24 +0200

    Reading and Updating related data

--- ContosoUniversity/Migrations/201507080810174_ComplexDataModel.Designer.cs
+++ ContosoUniversity/Migrations/201507080810174_ComplexDataModel.Designer.cs
@@ -7,13 +7,13 @@ namespace ContosoUniversity.Migrations
     using System.Resources;
     
     [GeneratedCode("EntityFramework.Migrations", "6.1.3-40302")]
-    public sealed partial class ColumnFirstName : IMigrationMetadata
+    public sealed partial class ComplexDataModel : IMigrationMetadata
     {
-        private readonly ResourceManager Resources = new ResourceManager(typeof(ColumnFirstName));
+        private readonly ResourceManager Resources = new ResourceManager(typeof(ComplexDataModel));
         
         string IMigrationMetadata.Id
         {
-            get { return "201507071248559_ColumnFirstName"; }
+            get { return "201507080810174_ComplexDataModel"; }
         }
         
         string IMigrationMetadata.Source
